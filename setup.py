8aaccb70bf2c45c98d6db35701f5051254b0abcb
Brad Riley <bradleeriley@gmail.com>
2021-10-18 20:18:07 -0500

    version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name='nvdlib',
     packages=find_packages(include=['nvdlib']),
-    version='0.5.0',
+    version='0.5.1',
     install_requires = ['requests'],
     description='National Vulnerability Database CPE/CVE API Library',
     author='Vehemont',
