fc59f70ebfb24e09c959a98689ff60baeb86469b
vehemont <48965163+vehemont@users.noreply.github.com>
2022-05-18 12:14:01 -0500

    Updated to 0.5.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name='nvdlib',
     packages=find_packages(include=['nvdlib']),
-    version='0.5.6',
+    version='0.5.7',
     install_requires = ['requests'],
     extras_require={
         "dev": [
