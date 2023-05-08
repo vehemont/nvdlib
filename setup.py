e4bcc98f956e987b3cb9c8d28bc39b9bb18a9f90
Brad <48965163+vehemont@users.noreply.github.com>
2023-05-08 15:39:16 -0500

    Updated docs and versioning

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name='nvdlib',
     packages=find_packages(include=['nvdlib']),
-    version='0.7.3',
+    version='0.7.4',
     install_requires = ['requests'],
     extras_require={
         "dev": [
