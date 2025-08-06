436a11f60f67cf557adb7100ef6d94234bb2af50
Noel Nunkovich <ntnunk@gmail.com>
2025-08-06 10:05:48 -0400

    Fixes issue #62

--- nvdlib/get.py
+++ nvdlib/get.py
@@ -1,19 +1,20 @@
-from typing import Literal, Dict, Mapping, Union, Optional, LiteralString, Generator, Any
+import logging
+import time
+from json.decoder import JSONDecodeError
+from typing import (Any, Dict, Generator, Literal, LiteralString, Mapping,
+                    Optional, Union)
 
 import requests
-import time
-import logging
-
-from json.decoder import JSONDecodeError
 
 logger = logging.getLogger(__name__)
 
+
 def __get(
-        product: Literal["cve", "cpe", "cpeMatch"],
-        headers: Mapping[str, Union[str, bytes, None]],
-        parameters: Dict[str, Union[str, LiteralString, int]],
-        limit: Optional[int] = None,
-        delay: Optional[float] = None
+    product: Literal['cve', 'cpe', 'cpeMatch'],
+    headers: Mapping[str, Union[str, bytes, None]],
+    parameters: Dict[str, Union[str, LiteralString, int]],
+    limit: Optional[int] = None,
+    delay: Optional[float] = None,
 ) -> Dict[str, Any]:
     """Calculate required pages for multiple requests, send the GET request with the search criteria, return list of CVEs or CPEs objects."""
 
@@ -28,8 +29,9 @@ def __get(
     # Requests doesn't really work with dictionary parameters that have no value like `isVulnerable`. The workaround is to just pass a string instead.
     # This joins the parameters into a string with '&' and if a key contains a value then it will join the values with '='
     stringParams = '&'.join(
-        [k if v is None else f"{k}={v}" for k, v in parameters.items()])
-    logger.debug("Filter:\n%s", link + stringParams)
+        [k if v is None else f'{k}={v}' for k, v in parameters.items()]
+    )
+    logger.debug('Filter:\n%s', link + stringParams)
 
     raw = requests.get(link, params=stringParams, headers=headers, timeout=30)
     raw.encoding = 'utf-8'
@@ -40,8 +42,8 @@ def __get(
         if 'message' in raw:
             raise LookupError(raw['message'])
     except JSONDecodeError:
-        logger.error("Invalid search criteria syntax: %s", str(raw))
-        logger.error("Attempted search criteria: %s", str(parameters))
+        logger.error('Invalid search criteria syntax: %s', str(raw))
+        logger.error('Attempted search criteria: %s', str(parameters))
 
     if delay is None:
         delay = 6
@@ -49,52 +51,58 @@ def __get(
 
     # If a limit is in the search criteria or the total number of results are less than or equal to the default 2000 that were just requested, return and don't request anymore.
     totalResults = raw['totalResults']
-    if limit is not None or totalResults <= 2000:
+    if limit is not None or totalResults <= raw['resultsPerPage']:
         return raw
 
     # If the results is more than the API limit, figure out how many pages there are and calculate the number of requests.
     # Use the page we already grabbed, then send a request starting at startIndex = 2000, then get the next page and ask for 2000 more results at the 2000th index result until all results have been grabbed.
     # Add each ['vulnerabilities'] or ['products'] list from each page to the end of the first request. Effectively creates one data point.
-    elif totalResults > 2000:
-        pages = (totalResults // 2000)
-        startIndex = 2000
+    elif totalResults > raw['resultsPerPage']:
+        pages = totalResults // raw['resultsPerPage']
+        startIndex = raw['resultsPerPage']
         if product == 'cve':
             path = 'vulnerabilities'
+        elif product == 'cpeMatch':
+            path = 'matchStrings'
         else:
             path = 'products'
 
         rawTemp = raw[path]
 
         for eachPage in range(pages):
-            parameters['resultsPerPage'] = '2000'
+            # parameters['resultsPerPage'] = '2000'
             parameters['startIndex'] = str(startIndex)
             stringParams = '&'.join(
-                [k if v is None else f"{k}={v}" for k, v in parameters.items()])
-            logger.debug("Filter:\n%s", link + stringParams)
+                [k if v is None else f'{k}={v}' for k, v in parameters.items()]
+            )
+            logger.debug('Filter:\n%s', link + stringParams)
             try:
                 getReq = requests.get(
-                    link, params=stringParams, headers=headers, timeout=30)
+                    link, params=stringParams, headers=headers, timeout=30
+                )
                 getReq.encoding = 'utf-8'
                 getData = getReq.json()[path]
                 time.sleep(delay)
             except JSONDecodeError:
                 logger.error('JSONDecodeError')
                 logger.error('Something went wrong: %s', str(getReq))
-                logger.error('Attempted search criteria: %s', str(stringParams))
+                logger.error(
+                    'Attempted search criteria: %s', str(stringParams)
+                )
                 logger.error('URL: %s', getReq.request.url)
                 getReq.raise_for_status()
             rawTemp.extend(getData)
-            startIndex += 2000
+            startIndex += raw['resultsPerPage']
         raw[path] = rawTemp
         return raw
 
 
 def __get_with_generator(
-        product: Literal["cve", "cpe", "cpeMatch"],
-        headers: Mapping[str, Union[str, bytes, None]],
-        parameters: Dict[str, Union[str, LiteralString, int]],
-        limit: Optional[int],
-        delay: float=6
+    product: Literal['cve', 'cpe', 'cpeMatch'],
+    headers: Mapping[str, Union[str, bytes, None]],
+    parameters: Dict[str, Union[str, LiteralString, int]],
+    limit: Optional[int],
+    delay: float = 6,
 ) -> Generator[Any, None, None]:
     # Get the default 2000 items to see the totalResults and determine pages required.
     if product == 'cve':
@@ -106,14 +114,19 @@ def __get_with_generator(
     startIndex = 0
     while True:
         stringParams = '&'.join(
-            [k if v is None else f"{k}={v}" for k, v in parameters.items()])
-        logger.debug("Filter:\n%s", link + stringParams)
+            [k if v is None else f'{k}={v}' for k, v in parameters.items()]
+        )
+        logger.debug('Filter:\n%s', link + stringParams)
         rate_delay = 1
         while True:
-            raw = requests.get(link, params=stringParams,
-                               headers=headers, timeout=30)
+            raw = requests.get(
+                link, params=stringParams, headers=headers, timeout=30
+            )
             if raw.status_code == 403:
-                logger.error("Request returned a rate limit error. Retrying in %f seconds...", rate_delay)
+                logger.error(
+                    'Request returned a rate limit error. Retrying in %f seconds...',
+                    rate_delay,
+                )
                 time.sleep(rate_delay)
                 rate_delay *= 2
             else:
@@ -127,24 +140,34 @@ def __get_with_generator(
             if 'message' in raw:
                 raise LookupError(raw['message'])
         except JSONDecodeError:
-            logger.error("Invalid search criteria syntax: %s", str(raw))
-            logger.error("Attempted search criteria: %s", str(parameters))
+            logger.error('Invalid search criteria syntax: %s', str(raw))
+            logger.error('Attempted search criteria: %s', str(parameters))
         yield raw
 
         totalResults = raw['totalResults']
 
-        startIndex += 2000
+        startIndex += raw['resultsPerPage']
         parameters['startIndex'] = str(startIndex)
-        parameters['resultsPerPage'] = '2000'
+        parameters['resultsPerPage'] = raw['resultsPerPage']
 
         if startIndex == 0:
-            logger.debug("Query returned %d total records", totalResults)
+            logger.debug('Query returned %d total records', totalResults)
 
         if limit is None:
             if startIndex < totalResults:
-                logger.debug("Getting %s batch %d to %d", product, raw["startIndex"], startIndex)
+                logger.debug(
+                    'Getting %s batch %d to %d',
+                    product,
+                    raw['startIndex'],
+                    startIndex,
+                )
             else:
-                logger.debug("Getting %s batch %d to %d", product, raw["startIndex"], totalResults)
+                logger.debug(
+                    'Getting %s batch %d to %d',
+                    product,
+                    raw['startIndex'],
+                    totalResults,
+                )
 
         if limit is not None or startIndex > totalResults:
             break
